308036e45d43e0d00b3f31e9e2acac4a9cb15744
Nicholas Pung <nckpng95@gmail.com>
2019-06-15 19:01:10 -0700

    Updates

---
+++ precache-manifest.b904f90725cd1464f84703d912bde3f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8285270c81022131f356ce1ccb571cfd",
+    "url": "/listOfManga/index.html"
+  },
+  {
+    "revision": "ccc8214c84d35936d38b",
+    "url": "/listOfManga/static/css/main.1ea15fd7.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/listOfManga/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "ccc8214c84d35936d38b",
+    "url": "/listOfManga/static/js/main.40f86376.chunk.js"
+  },
+  {
+    "revision": "15c0545940106e6b1a29",
+    "url": "/listOfManga/static/js/runtime~main.b72a1a18.js"
+  }
+]);
